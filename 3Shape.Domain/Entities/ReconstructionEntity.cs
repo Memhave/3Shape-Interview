04c3234aa942d9940ffb598c2aefa5afc70dbc98
Memhave <memhave@protonmail.ch>
2023-11-09 01:58:32 +0100

    Refactor ReconstructionEntity

--- 3Shape.Domain/Entities/ReconstructionEntity.cs
+++ 3Shape.Domain/Entities/ReconstructionEntity.cs
@@ -5,17 +5,24 @@ namespace _3Shape.Domain.Entities;
 
 public class ReconstructionEntity
 {
-    public Guid Id { get; init; }
-    public string Image { get; init; }
+    public Guid Id { get; private set; }
+    public string Image { get; private set; }
     // This record could be another entity, but to keep it simple
     // Idea is to be able to step "backwards" easily, without doing expensive
     // string manipulation or complex operations
     // So you could simply do entity.Stepback(N) -> would minus the last two (in order)
     // additions to the image
     // this might be a very complex or over engineered solution and might require some thread protection
-    public Dictionary<int, string> Steps {get; init; }
+    public Dictionary<int, string> Steps {get; private set; }
 
-    // Validation could be done through a constructor here, or a dedicated method
+    public ReconstructionEntity(Guid id, string initialScan)
+    {
+        // Validation
+
+        Id = id;
+        Image = initialScan;
+        Steps.Add(0, initialScan);
+    }
 
     public string GetToothScan(int toothId)
     {
@@ -23,7 +30,7 @@ public class ReconstructionEntity
         return "";
     }
 
-    public string AddStep(string part)
+    public void AddStep(string scan)
     {
         // Attach it to the image (align with overlap)
         // We probably need to check for deviation - 
@@ -31,7 +38,13 @@ public class ReconstructionEntity
         // but with ed2...? (c -> e)
 
         // Then increment steps 
+        Steps.Add(Steps.Count, scan);
         // if the deviation is allowed, it should probably be noted
-        return "";
     }
+
+    public void Trim(int stepCount)
+    {
+        // walk back steps here
+    }
+
 }
